33ed7455632e24d0caae4b902e4c27f1189e610d
hoangphucloc <hoangloc0706@gmail.com>
2024-10-10 23:48:45 +0700

    Implement display posts with the most views and upvotes

--- NewsAggregation/NewsAggregator/Controllers/PostController.cs
+++ NewsAggregation/NewsAggregator/Controllers/PostController.cs
@@ -9,6 +9,69 @@ namespace NewsAggregator.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
-        
+        private readonly string _connectionString;
+
+        public PostController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        [HttpGet("popular")]
+        public async Task<ActionResult<IEnumerable<Post>>> GetPopularPosts()
+        {
+            var posts = new List<Post>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SqlCommand("SELECT TOP 10 Id, Title, ViewCount " +
+                    "FROM Post " +
+                    "ORDER BY ViewCount DESC", connection);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        posts.Add(new Post
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            ViewCount = reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+
+            return Ok(posts);
+        }
+
+        [HttpGet("upvoted")]
+        public async Task<ActionResult<IEnumerable<Post>>> GetTopUpvotedPosts()
+        {
+            var posts = new List<Post>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var command = new SqlCommand("SELECT TOP 10 Id, Title, Upvote " +
+                    "FROM Post " +
+                    "ORDER BY Upvote DESC", connection);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        posts.Add(new Post
+                        {
+                            Id = reader.GetInt32(0),
+                            Title = reader.GetString(1),
+                            Upvote = reader.GetInt32(2)
+                        });
+                    }
+                }
+            }
+
+            return Ok(posts);
+        }
     }
 }
